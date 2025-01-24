95fa8a8380b961442756b62f2531b97231c0dcc0
ikurbanov429 <ikurbanov429@gmail.com>
2025-01-24 14:39:50 +0500

    app.py

--- app.py
+++ app.py
@@ -67,4 +67,5 @@ def index():
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))  # Render использует переменную PORT
-    app.run(host
+    app.run(host="0.0.0.0", port=port)
+  
